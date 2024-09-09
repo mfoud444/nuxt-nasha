9bdef8145c97c34ecfc462c476549bf9049e66d7
Mohammed Foud <mfoud444@gmail.com>
2024-09-10 02:49:52 +0300

    i commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nasha.store',
   appName: 'Nasha Store',
-  webDir: '.output/public'
+  webDir: 'dist'
 };
 
 export default config;
