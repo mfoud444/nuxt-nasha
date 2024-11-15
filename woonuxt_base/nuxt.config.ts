443e34aed391e430f03439e21a17ba06cc758bb9
Mohammed Foud <mfoud444@gmail.com>
2024-11-16 01:30:29 +0300

    i commit

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -58,14 +58,14 @@ export default defineNuxtConfig({
 
 
 
-  // 'graphql-client': {
-  //   clients: {
-  //     default: {
-  //       host: process.env.GQL_HOST || 'http://localhost:4000/graphql',
-  //       corsOptions: { mode: 'cors', credentials: 'include' },
-  //     },
-  //   },
-  // },
+  'graphql-client': {
+    clients: {
+      default: {
+        host: process.env.GQL_HOST || 'http://localhost:4000/graphql',
+        corsOptions: { mode: 'cors', credentials: 'include' },
+      },
+    },
+  },
 
 
   alias: {
