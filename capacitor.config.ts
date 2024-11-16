db20d0468ae53f9004c2ffa4439d98fce5710f26
Mohammed Foud <mfoud444@gmail.com>
2024-11-16 03:01:39 +0300

    i commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="@capacitor-community/safe-area" />
 import type { CapacitorConfig } from '@capacitor/cli';
 
+
 const config: CapacitorConfig = {
   appId: 'com.nasha.store',
   appName: 'Nasha Store',
